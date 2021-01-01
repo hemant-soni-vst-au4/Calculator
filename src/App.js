5d7dfa56cda5aef6e080c49fc744098c5ca6a02c
hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-01-01 23:21:45 +0530

    react setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello</h1>
-      </div>
-    )
-  }
-}
+const App = () => (
+  <div>
+    <h1>Calculator</h1>
+  </div>
+);
 
+export default App;
