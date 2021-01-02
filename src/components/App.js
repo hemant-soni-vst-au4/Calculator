3735f5f4a1ddf9922b84fe8ea5dab75298dc2588
hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-01-03 00:03:43 +0530

    linter error resolved

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
-import calculate from '../logic/calculate' // eslint-disable-next-line no-unused-vars
+import calculate from '../logic/calculate'; // eslint-disable-line
+
 const App = () => (
   <>
     <Display />
